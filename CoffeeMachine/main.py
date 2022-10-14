d2e648b61c1a5834713c36b2f394474b835d53b7
techsagemc <techsagemc@gmail.com>
2022-10-14 14:06:01 -0400

    define a few functions for code body

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -23,7 +23,7 @@ MENU = {
         "cost": 3.0,
     }
 }
-
+Money = 0
 resources = {
     "water": 300,
     "milk": 200,
@@ -33,11 +33,71 @@ resources = {
 
 # TODO: 1. Print report to show current resource
 
-choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+def print_resources():
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f'Money: ${Money}')
+      
+    
+        
+def check_ingredients(drink):
+    for ingredient, amount in drink['ingredients'].items():
+        if resources[ingredient] < amount:
+            print("Sorry, not enough ingredients")
+            coffee_machine()
+       
+            
+
+def count_money():
+    
+    quarters = .25 * num_of_quarters
+    dimes = .10 * num_of_dimes
+    nickels = .05 * num_of_nickels
+    pennies = .01 * num_of_pennies
+    
+ 
 
 
-def check_resources():
-    if choice == "report":
-        return MENU[resources]
-    elif choice == "espresso":
-        return MENU
+
+def change(cost):
+    for item, value in MENU[cost]:
+        change = quarters + dimes + nickels + pennies
+        
+          
+
+
+def coffee_machine():
+    is_running = True
+    while is_running:
+        choice = input("What would you like? (espresso/latte/cappuccino): ").lower()
+        if choice == 'off':
+            is_running = False
+            print("Turning off the machine")
+        
+        elif choice == "report":
+            print_resources()   
+            coffee_machine()
+        
+        print("Please insert coins.")
+        num_of_quarters = int(input('How many quarters?'))
+        num_of_dimes = int(input('How many dimes?'))
+        num_of_nickels = int(input('How many nickels?'))
+        num_of_pennies = int(input('How many pennies?'))
+        
+        count_money()
+        check_ingredients()
+        change(cost = choice)
+        
+        print(f"Here is your {choice}☕. Enjoy!")
+        is_running = False
+        
+coffee_machine()    
+    
+             
+    
+    # TODO: 2. Check if there is enough resources to make the drink. If not print "Sorry, not enough resources" and continue.
+    
+    
+
